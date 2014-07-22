f928800ebbc0c117417058eb93cc44c21b024fae
Krishn Ramesh <krishnr@gmail.com>
2014-07-21 22:19:07 -0400

    set up interrupts

--- elevator_code/elevator_code.ino
+++ elevator_code/elevator_code.ino
@@ -1,17 +1,21 @@
 int currentFloor = 0;
 int destinationFloor = 0;
-const int floor0Pin = 2;
-const int floor1Pin = 3;
-const int floor2Pin = 4;
-const int floor3Pin = 5;
+const int floor0Pin = 4;
+const int floor1Pin = 5;
+const int floor2Pin = 6;
+const int floor3Pin = 7;
 const int floor0LED = 10;
 const int floor1LED = 11;
 const int floor2LED = 12;
 const int floor3LED = 13;
+const int doorClosePin = 2;
+const int doorOpenPin = 3;
+const int expressPin = 8;
+const int emergencyStopPin = 0;
 volatile int overflowCount=0;
-volatile int millisCount=0;
-volatile float fraction = 0;
-volatile unsigned long m = 0;
+const int difference = floor0LED;
+const int pauseTime = 750;
+const int doorTime = 500;
 
 void setup() {
   pinMode(floor0Pin, INPUT_PULLUP);
@@ -22,10 +26,24 @@ void setup() {
   pinMode(floor1LED, OUTPUT);
   pinMode(floor2LED, OUTPUT);
   pinMode(floor3LED, OUTPUT);
-
+  
+  digitalWrite(doorClosePin, HIGH);
+  attachInterrupt(0, doorClose, CHANGE);
+  
+  digitalWrite(doorOpenPin, HIGH);
+  attachInterrupt(1, doorOpen, CHANGE);
+  
+  digitalWrite(expressPin, HIGH);
+  PCICR |= _BV(PCIE0);
+  PCMSK0 = _BV(PCINT0);
+  
+  digitalWrite(emergencyStopPin, HIGH);
+  PCICR |= _BV(PCIE2);
+  PCMSK2 = _BV(PCINT16);
+  
 
   TIMSK1=0x01; // enabled global and timer overflow interrupt;
-  TCCR1A = 0x00; // normal operation page 148 (mode0);
+  TCCR1A = 0x00; // normal operation mode
   TCNT1=0x0BDC; // set initial value to remove time error (16bit counter register)
   TCCR1B = 0x04; // start timer/ set clock
   
@@ -35,20 +53,30 @@ void setup() {
 
 void loop() {
   
-  digitalWrite(currentFloor+10, HIGH);
+  
+  digitalWrite(currentFloor+difference, HIGH);
+  long startTime = time();
   
     if(digitalRead(floor0Pin)==LOW){
       destinationFloor=0;
-      Serial.println("Going to floor 0");
+      if(destinationFloor != currentFloor){
+        Serial.println("Going to floor 0");
+      }
     }else if(digitalRead(floor1Pin)==LOW){
       destinationFloor=1;
-      Serial.println("Going to floor 1");
+      if(destinationFloor != currentFloor){
+        Serial.println("Going to floor 1");
+      }
     }else if(digitalRead(floor2Pin)==LOW){
       destinationFloor=2;
-      Serial.println("Going to floor 2");
+      if(destinationFloor != currentFloor){
+        Serial.println("Going to floor 2");
+      }
     }else if(digitalRead(floor3Pin)==LOW){
       destinationFloor=3;
-      Serial.println("Going to floor 3");
+      if(destinationFloor != currentFloor){
+        Serial.println("Going to floor 3");
+      }
     }
   
   if(destinationFloor != currentFloor) {
@@ -57,13 +85,13 @@ void loop() {
       
       for(int i = currentFloor; i < destinationFloor; i++) {
         //wait for travel time
-        digitalWrite(i+9, LOW);
+        digitalWrite(i+difference-1, LOW);
         pause(750);
-        digitalWrite(i+10, HIGH);
+        digitalWrite(i+difference, HIGH);
         Serial.print("On floor ");
         Serial.print(i);
         Serial.println();
-        pause(750);
+        pause(pauseTime);
       }
     }  
     
@@ -72,26 +100,30 @@ void loop() {
       
       for(int i = currentFloor; i > destinationFloor; i--) {
         //wait for travel time
-        digitalWrite(i+11, LOW);
-        pause(750);
-        digitalWrite(i+10, HIGH);
+        digitalWrite(i+difference+1, LOW);
+        pause(pauseTime);
+        digitalWrite(i+difference, HIGH);
         Serial.print("On floor ");
         Serial.println(i);
-        pause(750);
+        pause(pauseTime);
       }
     } 
     
-    digitalWrite(destinationFloor+9, LOW);
-    digitalWrite(destinationFloor+11, LOW);
-    pause(750);
-    Serial.print("Arrived at ");
+    digitalWrite(destinationFloor + difference -1, LOW);
+    digitalWrite(destinationFloor + difference +1, LOW);
+    pause(pauseTime);
+    Serial.print("Arrived at floor ");
     Serial.println(destinationFloor);
+    Serial.print("Your total trip time was ");
+    Serial.print((time() - startTime)/1000.0);
+    Serial.println(" seconds.");
     Serial.println();
     
     //open and close door
     
     currentFloor = destinationFloor;
-  }
+    
+    }
 }
 
 ISR(TIMER1_OVF_vect)
@@ -112,7 +144,7 @@ unsigned long time()
     n = TCNT1 + (overflowCount * 62500);
     n = n/62500.0*1000.0;
     SREG = oldSREG; 
-    
+    sei();
     
     return n;
 }
@@ -121,8 +153,23 @@ void pause(int delayTime)
 {
   long startTime = time();
   
-  
   while(time() <= startTime+delayTime) {}
 
   return;
 }  
+
+void doorClose() {
+  
+}
+
+void doorOpen() {
+  
+}
+
+ISR (PCINT0_vect) {
+
+}
+
+ISR (PCINT2_vect) {
+  
+}
